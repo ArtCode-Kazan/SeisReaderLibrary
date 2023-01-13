6804beb9d79402b6033b20f9e94add558c3386e7
fotuka <antda08@yandex.ru>
2023-01-13 15:52:21 +0300

    Beginning

---
+++ MiniProjectForTestLibrary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinReader;
+
+namespace MiniProjectForTestLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "D:/Binaryfiles/HF_0006_2020-02-22_00-00-00_6247_258.00";
+            BinarySeismicFile binfile = new BinarySeismicFile(path);
+            Int32[] signalArray = binfile.GetComponentSignal("Z");
+            FileHeader fileHat = binfile._FileHeader;
+            Console.WriteLine(fileHat.channelCount);
+            Console.WriteLine(fileHat.datetimeStart);
+            Console.WriteLine(fileHat.frequency);
+            Console.WriteLine(fileHat.latitude);
+            Console.WriteLine(fileHat.longitude);
+            Console.ReadLine();
+        }
+    }    
+}
